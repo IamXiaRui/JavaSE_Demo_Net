2c5cc79bc4cdd7f0d535ce40b72f04d85558caa6
IamXiaRui <iamxiarui@foxmail.com>
2016-03-08 00:50:16 +0800

    利用TCP实现数据传输

---
+++ xr_java_net/src/xr_net_tcp/TcpReceiveTest.java
@@ -0,0 +1,37 @@
+package xr_net_tcp;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/*
+ * 利用TCP 实现服务端数据接收功能
+ * 
+ * */
+public class TcpReceiveTest {
+
+	public static void main(String[] args) throws Exception {
+		// 建立服务端的Socket 并指定客户端的端口号
+		ServerSocket ss = new ServerSocket(8025);
+
+		// 获取连接过来的客户端对象
+		Socket s = ss.accept();
+
+		// 得到客户端发来的数据信息流
+		InputStream in = s.getInputStream();
+		byte[] buf = new byte[1024];
+		int length = in.read(buf);
+
+		System.out.println(new String(buf, 0, length));
+
+		// 向客户端发送反馈信息
+		OutputStream out = s.getOutputStream();
+		out.write("i am ruixia".getBytes());
+
+		s.close(); // 可以关闭客户端中的对象
+		ss.close();// 可选操作 一般服务器端不关闭
+
+	}
+
+}
---
+++ xr_java_net/src/xr_net_tcp/TcpSendTest.java
@@ -0,0 +1,34 @@
+package xr_net_tcp;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+/*
+ * 利用TCP 实现客户端数据发送功能
+ * 
+ * 需要先开服务端  否则客户端将无法发送数据
+ * */
+public class TcpSendTest {
+
+	public static void main(String[] args) throws Exception {
+		// 建立Socket服务 指定主机号 和 端口号
+		Socket s = new Socket(InetAddress.getLocalHost(), 8025);
+
+		// 得到输入流
+		OutputStream out = s.getOutputStream();
+		out.write("i am xiarui".getBytes());
+
+		// 接收服务端 反馈的数据信息流
+		InputStream in = s.getInputStream();
+		byte[] buf = new byte[1024];
+		int length = in.read(buf);
+
+		System.out.println(new String(buf, 0, length));
+
+		s.close();
+
+	}
+
+}
