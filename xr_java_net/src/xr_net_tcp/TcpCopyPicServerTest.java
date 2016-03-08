537a88d6afcab15464d41943e64bd9bd5e28d36a
IamXiaRui <iamxiarui@foxmail.com>
2016-03-08 20:38:08 +0800

    并发处理客户端请求

--- xr_java_net/src/xr_net_tcp/TcpCopyPicServerTest.java
+++ xr_java_net/src/xr_net_tcp/TcpCopyPicServerTest.java
@@ -1,9 +1,11 @@
 package xr_net_tcp;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 /*
  * 利用TCP  实现图片的复制
  * 
- * 服务端
+ * 服务端   并发处理客户端请求
  * 
  * */
 import java.io.FileOutputStream;
@@ -20,29 +22,64 @@ public class TcpCopyPicServerTest {
 		// 创建服务Socket对象 并指定端口号
 		ServerSocket ss = new ServerSocket(10001);
 
-		// 绑定客户端的Socket
-		Socket s = ss.accept();
-		// 数据读取流
-		InputStream is = s.getInputStream();
-		// 将读取的数据写入文件中
-		FileOutputStream fo = new FileOutputStream("E:\\AllTestFile\\TcpCopyPic.png");
+		while (true) {
+			// 绑定客户端的Socket
+			Socket s = ss.accept();
 
-		// 读取得到的文件数据
-		byte[] b = new byte[1024];
-
-		int len = 0;
-
-		while ((len = is.read(b)) != -1) {
-			fo.write(b, 0, len);
+			// 创建并发处理客户端请求的服务端
+			new Thread(new PicServer(s)).start();
 		}
+	}
+}
 
-		// 发送给客户端的消息流
-		OutputStream os = s.getOutputStream();
+class PicServer implements Runnable {
 
-		os.write("复制成功".getBytes());
+	private Socket s;
 
-		fo.close();
-		ss.close();
-		s.close();
+	public PicServer(Socket s) {
+		this.s = s;
+	}
+
+	@Override
+	public void run() {
+
+		int count = 1;
+		try {
+
+			File file = new File("E:\\AllTestFile\\TcpCopyPic-" + count + ".png");
+
+			while (file.exists()) {
+				file = new File("E:\\AllTestFile\\TcpCopyPic-" + (count++) + ".png");
+			}
+			// 数据读取流
+			InputStream is = s.getInputStream();
+			// 将读取的数据写入文件中
+			FileOutputStream fo = new FileOutputStream(file);
+
+			// 读取得到的文件数据
+			byte[] b = new byte[1024];
+
+			int len = 0;
+
+			while ((len = is.read(b)) != -1) {
+				fo.write(b, 0, len);
+			}
+
+			// 发送给客户端的消息流
+			OutputStream os = s.getOutputStream();
+
+			os.write("复制成功".getBytes());
+
+			fo.close();
+
+		} catch (FileNotFoundException e)
+
+		{
+			System.out.println(e.toString());
+		} catch (IOException e)
+
+		{
+			System.out.println(e.toString());
+		}
 	}
 }
